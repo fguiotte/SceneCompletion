0d069e07340cf8041c528fd35fe9374660f6cae1
Karamaz0v1 <florent.guiotte@gmail.com>
2015-11-10 14:45:34 +0100

    Init cpp project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/***************************************************************************
+ *
+ * TODO: Project Title
+ *
+ * Author: Florent Guiotte <guiotte.florent@gmail.com>
+ *
+ **************************************************************************/
+
+#include <stdio.h>
+
+int main( int argc, char* argv[] )
+{
+    int ret = 0;
+    
+
+    return ret;
+}
+
+
