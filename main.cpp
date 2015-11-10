46ebef991ef26b4ab97fcab1acaabbea541f6ff2
Karamaz0v1 <florent.guiotte@gmail.com>
2015-11-10 15:58:30 +0100

    OpenCV disp image

--- main.cpp
+++ main.cpp
@@ -8,10 +8,20 @@
 
 #include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 int main( int argc, char* argv[] )
 {
-    cv::grabCut();
+    cv::Mat src;
+    cv::Mat dst;
+    char window_name[] = "Demo 1";
+
+    src = cv::imread("../img/lena.jpg", 1);
+    cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
+    imshow(window_name, src);
+    cv::waitKey(0);
+
+    //cv::grabCut();
 
     return 0;
 }
